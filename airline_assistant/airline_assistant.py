75a0c38c478bea95252662714b5abb3b601de1d3
Kanwar Taimoor <kanwartaimoor99@gmail.com>
2025-05-20 04:58:01 -0700

    multi-modal-airline assistant

--- airline_assistant/airline_assistant.py
+++ airline_assistant/airline_assistant.py
@@ -4,6 +4,12 @@ import json
 from dotenv import load_dotenv
 from openai import OpenAI
 import gradio as gr
+import base64
+from io import BytesIO
+from PIL import Image
+from pydub import AudioSegment
+from pydub.playback import play
+import traceback
 
 # Initialization
 load_dotenv(override=True)
@@ -52,20 +58,26 @@ price_function = {
 tools = [{"type": "function", "function": price_function}]
 
 
-def chat(message, history):
-    messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
-    response = openai.chat.completions.create(model=MODEL, messages=messages, tools=tools) # see here we are passing the list of available tools 
-
-    if response.choices[0].finish_reason=="tool_calls": #gpt sends finish_reason when it says it doesnt know the answer and now wants to user our tool and sends the "tool_calls option"
+def chat(history):
+    messages = [{"role": "system", "content": system_message}] + history
+    response = openai.chat.completions.create(model=MODEL, messages=messages, tools=tools)
+    image = None
+    
+    if response.choices[0].finish_reason=="tool_calls":
         message = response.choices[0].message
         response, city = handle_tool_call(message)
-
-        #here we need to append two new messages the message sent by LLM to call tool and then our response 
         messages.append(message)
         messages.append(response)
+        image = artist(city)
         response = openai.chat.completions.create(model=MODEL, messages=messages)
+        
+    reply = response.choices[0].message.content
+    history += [{"role":"assistant", "content":reply}]
+
+    # Comment out or delete the next line if you'd rather skip Audio for now..
+    talker(reply)
     
-    return response.choices[0].message.content
+    return history, image
 
 # We have to write that function handle_tool_call:
 def handle_tool_call(message):
@@ -80,4 +92,51 @@ def handle_tool_call(message):
     }
     return response, city
 
-gr.ChatInterface(fn=chat, type="messages").launch()
+
+#image generation tool 
+def artist(city):
+    image_response = openai.images.generate(
+            model="dall-e-3",
+            prompt=f"An image representing a vacation in {city}, showing tourist spots and everything unique about {city}, in a vibrant pop-art style",
+            size="1024x1024",
+            n=1, #how many images
+            response_format="b64_json",
+        )
+    image_base64 = image_response.data[0].b64_json
+    image_data = base64.b64decode(image_base64)
+    return Image.open(BytesIO(image_data))
+
+#text to audio tool 
+def talker(message):
+    response = openai.audio.speech.create(
+      model="tts-1",
+      voice="onyx",    # Also, try replacing onyx with alloy
+      input=message
+    )
+    
+    audio_stream = BytesIO(response.content)
+    audio = AudioSegment.from_file(audio_stream, format="mp3")
+    play(audio)
+
+# More involved Gradio code as we're not using the preset Chat interface!
+# Passing in inbrowser=True in the last line will cause a Gradio window to pop up immediately.
+
+with gr.Blocks() as ui:
+    with gr.Row():
+        chatbot = gr.Chatbot(height=500, type="messages")
+        image_output = gr.Image(height=500)
+    with gr.Row():
+        entry = gr.Textbox(label="Chat with our AI Assistant:")
+    with gr.Row():
+        clear = gr.Button("Clear")
+
+    def do_entry(message, history):
+        history += [{"role":"user", "content":message}]
+        return "", history
+
+    entry.submit(do_entry, inputs=[entry, chatbot], outputs=[entry, chatbot]).then(
+        chat, inputs=chatbot, outputs=[chatbot, image_output]
+    )
+    clear.click(lambda: None, inputs=None, outputs=chatbot, queue=False)
+
+ui.launch(inbrowser=True)
