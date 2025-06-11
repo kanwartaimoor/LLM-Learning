f5673901127519a81221ae2f188ee05792e869cc
Kanwar Taimoor <kanwartaimoor99@gmail.com>
2025-06-11 05:08:55 -0700

    tokensiers

--- airline_assistant/airline_assistant.py
+++ airline_assistant/airline_assistant.py
@@ -97,6 +97,7 @@ def handle_tool_call(message):
 def artist(city):
     image_response = openai.images.generate(
             model="dall-e-3",
+            ?
             prompt=f"An image representing a vacation in {city}, showing tourist spots and everything unique about {city}, in a vibrant pop-art style",
             size="1024x1024",
             n=1, #how many images
